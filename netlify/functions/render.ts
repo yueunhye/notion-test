4ae83f73f73a264e9b7caff84efc51fb83fefd7d
yueunhye <yulll035@naver.com>
2022-07-12 19:45:23 +0900

    & -> $

--- netlify/functions/render.ts
+++ netlify/functions/render.ts
@@ -55,7 +55,7 @@ const handler: Handler = async (event) => {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        &{
+        ${
           MODE === 'development' 
             ? '<script type="module" src="/src/main.ts"></script>' 
             : '<script type="module" crossorigin src="/assets/index.js"></script>' + 
